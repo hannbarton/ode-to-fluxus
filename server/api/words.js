ae2591f61235c9a789d05c348320e5c2f7632845
Hannah Barton <hannbarton@gmail.com>
2019-01-08 14:54:45 -0500

    twitter responding back on backend

--- server/api/words.js
+++ server/api/words.js
@@ -60,7 +60,8 @@ client.get('favorites/list', function(error, tweets, response) {
 	if(error) throw error;
 	console.log(tweets);  // The favorites.
 	// console.log(response);  // Raw response object.
-});
+
+})
 
 router.post('/', async(req, res, next) => {
     try{
