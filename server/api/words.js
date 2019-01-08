84abfd3ec1a52bbfe8eb08882efebb4e56ebc160
Hannah Barton <hannbarton@gmail.com>
2019-01-08 14:00:16 -0500

    integrated evn.process keys

--- server/api/words.js
+++ server/api/words.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 const {User, Word} = require('../db/models')
 module.exports = router
+var Twitter = require('twitter');
+
+// var client = new Twitter({
+//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+//   });
+
 
 router.get('/', async (req, res, next) => {
   try {
@@ -22,6 +31,35 @@ router.get('/:id', async (req, res, next) => {
 	} catch (err) {
 		next(err);
 	}
+});
+
+// app.get('/search', async (req, res, next) => {
+// 	try {
+// 		let client = new Twitter({
+// 			consumer_key: process.env.TWITTER_CONSUMER_KEY,
+// 			consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+// 			access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+// 			access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// 		  });
+// 		  let tweet = await client.get('/search?q=javascript')
+// 		  res.json(tweet.data)
+// 	}
+// 	catch (err) {
+// 		next(err)
+// 	}
+// })
+
+let client = new Twitter({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+client.get('favorites/list', function(error, tweets, response) {
+	if(error) throw error;
+	console.log(tweets);  // The favorites.
+	// console.log(response);  // Raw response object.
 });
 
 router.post('/', async(req, res, next) => {
