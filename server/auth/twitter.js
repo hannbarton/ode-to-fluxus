b12cefa62bd6678275257f1c605386b3e5418271
Hannah Barton <hannbarton@gmail.com>
2019-01-14 16:37:36 -0500

    formatting

--- server/auth/twitter.js
+++ server/auth/twitter.js
@@ -41,25 +41,25 @@ if (!process.env.TWITTER_CONSUMER_KEY || !process.env.TWITTER_CONSUMER_SECRET) {
           }
           return done(null, user)
         }
+        )
+      })
       )
-    })
-  )
 
-  router.get('/', passport.authorize('twitter', {scope: 'email'}))
+      router.get('/', passport.authorize('twitter', {scope: 'email'}))
 
-  router.get(
-    '/callback',
-    passport.authorize('twitter', {
-      successRedirect: '/poem',
-      failureRedirect: '/login',
-      failureFlash: true
-    }),
-    function(req, res) {
-      console.log('this is the callback req', req)
-      res.redirect('/')
-      console.log('req', req)
-      console.log('res', res)
-      // Associate the Twitter account with the logged-in user
-    }
-  )
+      router.get(
+        '/callback',
+        passport.authorize('twitter', {
+          successRedirect: '/poem',
+          failureRedirect: '/login',
+          failureFlash: true
+        }),
+        function(req, res) {
+          console.log('this is the callback req', req)
+          res.redirect('/')
+          console.log('req', req)
+          console.log('res', res)
+          // Associate the Twitter account with the logged-in user
+        }
+      )
 }
