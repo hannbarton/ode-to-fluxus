ba4e484aa57f2935f2a3648bf2b09b5f5944f068
Hannah Barton <hannbarton@gmail.com>
2019-01-15 11:32:50 -0500

    handle endpoints

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const passport = require('passport')
 module.exports = router
 
 router.post('/logout', (req, res) => {
@@ -19,6 +20,13 @@ router.post('/logout', (req, res) => {
 //   }
 //   return res.json({ error: 'User is not authenticated' });
 // })
+
+// need to protect endpoints?
+// router.get('/me',
+//   passport.authenticate('bearer', { session: false }),
+//   function(req, res) {
+//     res.json(req.user);
+//   });
 
 router.use('/google', require('./google'))
 router.use('/twitter', require('./twitter'))
