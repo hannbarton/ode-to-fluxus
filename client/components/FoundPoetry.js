dc5b621eb873bf6bf42652a1a2667a70cfb97125
Hannah Barton <hannbarton@gmail.com>
2019-01-21 11:31:12 -0500

    added found poetry react component with circles no konva

--- client/components/FoundPoetry.js
+++ client/components/FoundPoetry.js
@@ -1,33 +1,126 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
 class FoundPoetry extends React.Component {
+  state = {
+    isDragging: false,
 
-        render(){
-          var circleStyle = {
-            padding:10,
-            margin:20,
-            display:"inline-block",
-            // backgroundColor: "blue",
-            borderStyle: 'solid',
-            borderColor: '#1f252d',
-            borderWidth: 2,
-            borderRadius: "50%",
-            width:100,
-            height:100,
-          };
+    originalX: 0,
+    originalY: 0,
 
-          return (
-            <div><div style={circleStyle}/>
+    translateX: 0,
+    translateY: 0,
 
-                <p>hellowowlrd</p>
-            </div>
-          );
+    lastTranslateX: 0,
+    lastTranslateY: 0
+  }
 
-          }
+  componentWillUnmount() {
+    window.removeEventListener('mousemove', this.handleMouseMove)
+    window.removeEventListener('mouseup', this.handleMouseUp)
+  }
+
+  handleMouseDown = ({clientX, clientY}) => {
+    window.addEventListener('mousemove', this.handleMouseMove)
+    window.addEventListener('mouseup', this.handleMouseUp)
+
+    if (this.props.onDragStart) {
+      this.props.onDragStart()
+    }
+
+    this.setState({
+      originalX: clientX,
+      originalY: clientY,
+      isDragging: true
+    })
+  }
+
+  handleMouseMove = ({clientX, clientY}) => {
+    const {isDragging} = this.state
+    const {onDrag} = this.props
+
+    if (!isDragging) {
+      return
+    }
+    this.setState(
+      prevState => ({
+        translateX: clientX - prevState.originalX + prevState.lastTranslateX,
+        translateY: clientY - prevState.originalY + prevState.lastTranslateY
+      }),
+      () => {
+        if (onDrag) {
+          onDrag({
+            translateX: this.state.translateX,
+            translateY: this.state.translateY
+          })
+        }
+      }
+    )
+  }
+
+  handleMouseUp = () => {
+    window.removeEventListener('mousemove', this.handleMouseMove)
+    window.removeEventListener('mouseup', this.handleMouseUp)
+
+    this.setState(
+      {
+        originalX: 0,
+        originalY: 0,
+        lastTranslateX: this.state.translateX,
+        lastTranslateY: this.state.translateY,
+
+        isDragging: false
+      },
+      () => {
+        if (this.props.onDragEnd) {
+          this.props.onDragEnd()
+        }
+      }
+    )
+  }
+  render() {
+
+    const {children} = this.props
+    const {translateX, translateY, isDragging} = this.state
+
+    return (
+        <div>
 
 
+      <Container
+        onMouseDown={this.handleMouseDown}
+        x={translateX}
+        y={translateY}
+        isDragging={isDragging}
+      >
+        {children}
+      </Container>
+      <div>
+          hello world
+      </div>
+      </div>
+    )
+  }
 }
+
+const Container = styled.div.attrs({
+    style: ({ x, y }) => ({
+      transform: `translate(${x}px, ${y}px)`
+    }),
+  })
+  `
+  color: palevioletred;
+  border: 2px solid black;
+  border-radius: 50%;
+  width: 500px
+  height: 500px
+  cursor: grab;
+
+    ${({ isDragging }) =>
+    isDragging && css`
+      opacity: 0.8;
+      cursor: grabbing;
+    `};
+  `;
 
 export default FoundPoetry
--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -6,10 +6,11 @@ import {FoundPoetry} from './'
 class HomePage extends React.Component {
   render() {
     return (
-      <div className="home-page">
+      // <div className="home-page">
+       <div>
         <Navbar />
         {/* <UserHome /> */}
-        <div className="inner-paper">
+        {/* <div className="inner-paper">
           <div className="home-links">
             <h2>Found Poetry</h2>
             <p>is a type of poetry created by taking words, phrases, and sometimes whole passages from other sources and reframing them by making changes in spacing and lines, or by adding or deleting text, thus imparting new meaning. The resulting poem can be defined as either treated: changed in a profound and systematic manner; or untreated: virtually unchanged from the order, syntax and meaning of the poem.</p>
@@ -21,8 +22,8 @@ class HomePage extends React.Component {
             <br />
             <br />
             <Login />
-          </div>
-        </div>
+          </div> */}
+
         <FoundPoetry />
       </div>
     )
