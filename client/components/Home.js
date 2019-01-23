ab74d7b4f0ad6db89b7e212794b8ad9db5ba179a
Hannah Barton <hannbarton@gmail.com>
2019-01-23 09:47:17 -0500

    found poetry is..."

--- client/components/Home.js
+++ client/components/Home.js
@@ -5,32 +5,19 @@ import {Link} from 'react-router-dom'
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <Navbar />
+      <div className='home-outer'>
+      <Navbar />
+      <br/>
         <div className="home">
-          <br />
-
-          <h1>Found Poetry</h1>
-          <p>is a type of poetry created by taking words, phrases,</p>
-          <p>and sometimes whole passages from other sources and reframing </p>
-          <p>them by making changes in spacing and lines, or by adding or </p>
-          <p>deleting text, thus</p>
-          <p>imparting new meaning. </p>
-          <p>The resulting poem can be defined as either treated: </p>
-          <p>changed in a profound and systematic manner; </p>
-          <p>or </p>
-          <p>untreated:</p>
-          <p>
-            virtually unchanged from the order, syntax and meaning of the poem.
-          </p>
-          <br />
-          <br />
-        </div>
-        <div className="home-container">
+          <h1>Found Poetry
+          is a type of poetry created by taking words, phrases, and sometimes whole passages from other sources and imparting new meaning.</h1>
+          <div className="home-container">
+          <br/>
           <Link to="/poem">Make a poem from trending hashtags</Link>
           <br />
           <br />
           <Login />
+        </div>
         </div>
       </div>
     )
